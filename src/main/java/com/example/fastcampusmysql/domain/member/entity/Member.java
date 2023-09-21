14b1d8055fee5020821ef0085cc6153037ffe02f
sunsik <sunsig21c@gmail.com>
2023-09-21 23:21:42 +0900

    jdbc를 이용한 데이터 저장 JpaRepository를 비슷하게 구현하려고 하기 때문에 save()메서드는 새로 저장 or 업데이트가 될 수 있다.
    jdbcTamplate을 사용하지 않고 NamedParameterJdbcTemplate 사용
    NamedParameterJdbcTemplate는 JdbcTamplate을 상속받지 않고 프로퍼티로 정의돼있음

---
+++ src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
@@ -0,0 +1,38 @@
+package com.example.fastcampusmysql.domain.member.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Member {
+	final private Long id;
+
+	private String nickName;
+
+	final private String email;
+
+	final private LocalDate birthDay;
+
+	final private LocalDateTime createdAt;
+
+	final private static Long NAME_MAX_LENGTH = 10L;
+	@Builder
+	public Member(Long id, String nickName, String email, LocalDate birthDay,
+		LocalDateTime createdAt) {
+		this.id = id;
+		this.email = Objects.requireNonNull(email);
+		this.birthDay = Objects.requireNonNull(birthDay);
+
+		validateNickName(nickName);
+		this.nickName = Objects.requireNonNull(nickName);
+
+		this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
+	}
+
+	void validateNickName(String nickName) {
+		if (nickName.length() < 1 || nickName.length() > NAME_MAX_LENGTH) throw new RuntimeException("최대 길이를 초과했습니다.");
+	}
+}
--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -1,5 +1,30 @@
 package com.example.fastcampusmysql.domain.member.service;
 
-public class MemberWriteService {
+import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class MemberWriteService {
+	private final MemberRepository memberRepository;
+	public Member create(RegisterMemberCommand command) {
+		/*
+			목표 - 회원정보 저장 (이메일, 닉네임, 생년월일)
+				- 닉네임은 10자를 남길 수 없다.
+			파라미터 - memberRegisterCommand
+			val member = Member.of(memberRegisterCommand)
+			memberRepository.save(member)
+		 */
+
+		Member member = Member.builder()
+			.nickName(command.nickName())
+			.birthDay(command.birthday())
+			.email(command.email())
+			.build();
+
+		return memberRepository.save(member);
+	}
 }
